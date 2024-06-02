a06e6e6c7b192d8a11d0e5b98a26c4ea1fccb878
otaki00 <omarbin382@gmail.com>
2024-06-02 11:36:15 +0300

    initialize the project by download initial packages, setup  firebase  and tailwindcss

---
+++ ta7derapp/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='  p-10 text-blue-500 font-mono text-4xl font-extrabold'>App  </h1>
+    </>
+  )
+}
+
+export default App
